82ca2bdad29711448d8c62be803c169a9c0f1f06
Paul Okello <paulokello273@gmail.com>
2021-02-03 14:02:01 +0300

    Category model

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'categorie'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
