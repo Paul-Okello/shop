aa6c2ab59e46b39570fc92a259a6ee50b248ded1
Paul Okello <paulokello273@gmail.com>
2021-02-03 14:14:05 +0300

    product model debugging

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -33,5 +33,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse("Product_detail", kwargs={"pk": self.pk})
+    
