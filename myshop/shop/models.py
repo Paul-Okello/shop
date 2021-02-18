097fde22859c24f8f2f05479755ffa291acf7f48
Paul Okello <paulokello273@gmail.com>
2021-02-18 11:25:04 +0300

    Debugging return value

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
-# Create your models here.
 class Category(models.Model):
+    
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
-    
+
     class Meta:
-        ordering = ('name',)
-        verbose_name = 'categorie'
-        verbose_name_plural = 'categories'
         
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse("shop: product_list_by_category", args=[self.slug])
-    
-    
+        return reverse('shop:product_list', args=[self.slug])
+
+
 class Product(models.Model):
     
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    id = models.CharField(max_length=200, primary_key=True)
+    category = models.ForeignKey(Category,related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -37,5 +40,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
+    def get_absolute_url(self,id):
         return reverse('shop:product_detail', args=[self.id, self.slug])
